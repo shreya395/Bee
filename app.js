705bec20c7d64ccc3c0c73c0ce4e3b216196c88a
Shreya Shukla <101269825+shreya395@users.noreply.github.com>
2023-12-11 19:16:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://shreya395:sdshukla9839@cluster0.dstwt84.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
+
+
+
